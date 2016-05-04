066ee370a1c3af788d19e2e911743460c6e6a4a8
Crisly Domingos <bg70.student.sunderland.ac.uk>
2016-05-04 21:58:39 +0100

    Trying to build appi from ground floor

--- server.js
+++ server.js
@@ -1,3 +1,51 @@
-fvasd,fczxnb,cvhnfbzx 
-sdvxcjh,bvjzxcbv
-#xvbmzxcbvkxcv
+var express = require("express");
+var cors = require("cors");
+var bodyparser = require("body-parser");
+var app = express();
+
+app.use(cors());
+app.use(bodyparser.urlencoded());
+
+app.get("/", function(req, res)
+{
+	res.send("<h3>Let Smash this down now</h3>");
+});
+
+
+/* app.post("/test", function(req, res)
+{
+	console.log(req.body);
+	
+	//checking what the user submitted...
+	if (req.body.favBrowser == "chrome")
+	{
+		res.send("Chrome!!!");
+		if (req.body.favname == "crisly")
+		{
+			res.send("I am genius");
+		}
+		else
+		{
+			res.send("You are alien from Italy")
+		}
+	}
+	else
+	{
+		res.send("Not Chrome");
+	}
+}); */
+
+app.post("/test", function(req, res)
+{
+	console.log(req.body);
+	if (req.body.favname == "crisly")
+	{
+		res.send("I am genius");
+	}
+	else
+	{
+		res.send("You are alien from Italy")
+	}
+});
+
+app.listen(3000);
